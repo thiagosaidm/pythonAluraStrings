b9259e9dd6ea9021282c6120da7de0f2c1c21b09
DevThiago <thiag.said@gmail.com>
2023-11-03 20:27:54 -0300

    "Manipulação de Strings"

---
+++ main.py
@@ -0,0 +1,17 @@
+#fatiamento
+
+nome = "Thiago Said"
+
+primeiro_nome = nome[0:7]
+print(primeiro_nome)
+
+#passa a variável - entre colchetes escolhe o primeiro index da string ":" até onde quer extrair o texto +1
+# o primeiro paramento é inclusivo, e o ultimo exclusivo, não inclui o caractere naquela posição
+
+segundo_nome = nome[7:11]
+print(segundo_nome)
+
+#strings são imutáveis, permitindo apenas a manipulação de uma cópia delas em variáveis
+
+url = 'https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100'
+print(url)
