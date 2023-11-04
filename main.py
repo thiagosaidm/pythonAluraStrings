5e9a266b3b705c0e1cedf0cdbe64493c405e007f
DevThiago <thiag.said@gmail.com>
2023-11-03 21:29:58 -0300

    "Manipulação de Strings - metodo find"

--- main.py
+++ main.py
@@ -15,3 +15,29 @@ print(segundo_nome)
 
 url = 'https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100'
 print(url)
+
+#metodo find() para encontrar o separador da url
+indice_interrogacao = url.find('?')
+print(indice_interrogacao)
+
+#separar a url origem da url parametros
+url_origem = url[0:indice_interrogacao]
+print(url_origem)
+
+#usando o metodo de fatiamento sem o segundo parametro ele pega até o final da string
+url_parametros = url[indice_interrogacao+1:]
+print(url_parametros)
+
+#definir o parâmetro que será buscado/extraído
+parametro_busca = "quantidade"
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+
+#verificando se há parametros no url parametros antes ou após o & - se for antes é origem depois é destino
+indice_e_comercial = url_parametros.find('&', indice_valor) #  o segundo parametro é onde começa
+if(indice_e_comercial == -1):
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+print(valor)
+
